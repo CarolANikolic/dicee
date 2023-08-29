62aee05afaf1772a4e036326969da79bd7fae966
Carol A. Nikolic <carolnikolic@gmail.com>
2023-08-29 13:41:56 +0200

    feat(home): add main title info

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -2,6 +2,7 @@ const Text = (type, content) => {
     const textType = document.createElement(type);
 
     textType.textContent = content;
+    textType.classList.add("textAlign")
 
     if (type === "h1") {
         textType.classList.add("title")
--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import Button from "./components/Button/index.js";
 import Text from "./components/Text/index.js";
 
 const mainContent = document.querySelector("main");
+const titleBox = document.createElement("div")
 
+titleBox.appendChild(Text("h1", "Welcome to Dicee!"))
+titleBox.appendChild(Text("p", "Roll de dice and Good Luck!"))
+titleBox.classList.add("titleBox");
+
+mainContent.appendChild(titleBox)
